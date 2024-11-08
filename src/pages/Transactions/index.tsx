3d90558c5836526864ac4fd9ac47070718232b74
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-11-08 16:14:19 -0300

    create transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,39 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
            <Header />
+            <Summary />
 
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/09/2024</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Contas de casa</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 4.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Casa</td>
+                            <td>14/09/2024</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
