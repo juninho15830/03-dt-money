f266cdc43ada9c45753ee790132ea7e3c1107ce5
Jair de Jesus Redigolo Junior <32220266+juninho15830@users.noreply.github.com>
2024-11-13 20:56:48 -0300

    update search form

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,13 +1,38 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from "react-hook-form";
+import * as z from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const searchFromSchema = z.object({
+    query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFromSchema>
 
 export function SearchForm() {
-    return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque por transações" />
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm<SearchFormInputs>({
+        resolver: zodResolver(searchFromSchema)
+    })
 
-            <button>
+    function handleSearchTransactions(data: SearchFormInputs) {
+        console.log(data)
+    }
+
+    return (
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input type="text"
+            placeholder="Busque por transações"
+            {...register('query')}
+        />
+
+            <button type="submit" disabled={isSubmitting}>
                 <MagnifyingGlass size={20}/>
+                Buscar
             </button>
         </SearchFormContainer>
     )
